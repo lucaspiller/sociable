8e12128b75ca86ff9e542367efa0759f4b94d6f7
sociable <sociable@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-05 16:29:09 +0000

    New version 3.5.1
    git-svn-id: http://plugins.svn.wordpress.org/sociable/trunk@160721 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sociable.php
+++ sociable.php
@@ -622,7 +622,7 @@ $sociable_known_sites = Array(
 	 
 	 'Blogplay' => Array(
 	 	'favicon' => 'blogplay.png',
-	 	'url' => 'http://blogplay.com/plugin',
+	 	'url' => 'http://blogplay.com',
 	 	'spriteCoordinates' => Array(199,73),
 	 	'supportsIframe' => false,
 	 ),
