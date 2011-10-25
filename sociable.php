217c8929e88a42cfcf04c69431191fec1c3eba49
sociable <sociable@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-25 18:27:14 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sociable/trunk@455600 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sociable.php
+++ sociable.php
@@ -3,7 +3,7 @@
 Plugin Name: Sociable
 Plugin URI: http://blogplay.com/plugin
 Description: Automatically add links on your posts, pages and RSS feed to your favorite social bookmarking sites. 
-Version: 4.0.2
+Version: 4.0.3
 Author: Blogplay
 Author URI: http://blogplay.com/
 
