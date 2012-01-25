1ce9f25e855b640079b3b4edc6182717626a9bad
sociable <sociable@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-25 16:49:04 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sociable/trunk@495253 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/async_call.js
+++ js/async_call.js
@@ -1,13 +1,22 @@
-jQuery(document).ready(function($) {
-
-	var data = {
-		link: window.location.href,
-		title: jQuery('title').html()
-	};
-	
-	jQuery.post( '/wp-content/plugins/sociable/includes/async_request.php', data, function(response) {
-		
-		eval(response);
-		
-	});
+jQuery(document).ready(function($) {
+
+
+
+	var data = {
+
+		link: window.location.href,
+
+		title: jQuery('title').html()
+
+	};
+	
+	 
+
+	jQuery.post(base_url_sociable+'includes/async_request.php', data, function(response) {
+		 
+		
+		eval(response);
+
+	});
+
 });
