46c91fcb4d31a3f2077bebf98ab1ac006f5c12bb
sociable <sociable@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-07 13:45:40 +0000

    git-svn-id: http://plugins.svn.wordpress.org/sociable/trunk@540918 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sociable.php
+++ sociable.php
@@ -1,55 +1,20 @@
 <?php
-
 /*
-
-
-
 Plugin Name: Sociable
-
-
-
 Plugin URI: http://blogplay.com/plugin
-
-
-
 Description: Automatically add links on your posts, pages and RSS feed to your favorite social bookmarking sites. 
-
-
-
-Version: 4.2.7
-
-
-
+Version: 4.2.8
 Author: Blogplay
-
-
-
 Author URI: http://blogplay.com/
-
-
-
 Copyright 2006 Peter Harkins (ph@malaprop.org)
-
-
-
 Copyright 2008-2009 Joost de Valk (joost@yoast.com)
-
-
-
 Copyright 2009-Present Blogplay.com (info@blogplay.com)
-
-
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
-
-
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-
-
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 */
 
 
@@ -411,9 +376,7 @@ function sociable_reset(){
     
     	'blogplay_tags' => 1, 
 
-		'version' =>'4.2.7',
-
-
+		'version' =>'4.2.8',
 
         'automatic_mode'         => 'on',
 
